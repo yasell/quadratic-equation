c08da2851eec54b6ad8a7ac3a74af3babf0e73e2
Raman Yasel <strogofcdm@gmail.com>
2018-09-30 21:07:52 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  array = equation.split(' ');
+
+  let a = Number(array[0]);
+  let b = parseInt(array[3] + array[4], 10);
+  let c = parseInt(array[7] + array[8], 10);
+
+  const D = Math.pow(b, 2) - 4 * a * c;
+
+  const x_1 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+  const x_2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+  let resultArray = [];
+  x_1 < x_2 ? resultArray = [x_1, x_2] : resultArray = [x_2, x_1];
+
+  return resultArray;
 }
